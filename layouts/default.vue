90eccf5dc5df2084bfbc5de9125264501bbda414
den <budden73@mail.ru>
2018-03-09 18:12:29 +0300

    вроде заработало после переноса

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,23 +2,23 @@
   <div>
     <b-navbar toggleable="md" variant="light">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-navbar-brand to="/">Nuxt.js</b-navbar-brand>
+      <b-navbar-brand to="/">Zя</b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
           <b-nav-item to="/" exact>Home</b-nav-item>
           <b-nav-item to="/public">Public</b-nav-item>
           <b-nav-item to="/secure">Secure</b-nav-item>
+          <b-nav-item to="/article-editor">Редактор статьи</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <template v-if="$auth.state.loggedIn">
             <b-nav-item-dropdown :text="$auth.user.name" right>
-              <b-dropdown-item @click="$auth.logout()">Logout</b-dropdown-item>
+              <b-dropdown-item @click="$auth.logout()">Выйти</b-dropdown-item>
             </b-nav-item-dropdown>
-            <b-img :src="picture" class="mt-1" rounded="circle" width="30px" height="30px" />
           </template>
           <template v-else>
-            <b-dropdown-item to="/login">Login</b-dropdown-item>
+            <b-dropdown-item to="/login">Войти</b-dropdown-item>
           </template>
         </b-navbar-nav>
       </b-collapse>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 
 module.exports = {
-  rootDir: resolve(__dirname, '../..'),
+  rootDir: __dirname,
   srcDir: __dirname,
   dev: false,
   render: {
